2d14e2c89925773a1ba8a2b9112583b152957213
veroux <verocknroll.manne@live.fr>
2013-11-13 14:04:51 -0500

    correction affichage tableau

--- ctrl_vue/partieEnCours/tableauDeJeu_view.php
+++ ctrl_vue/partieEnCours/tableauDeJeu_view.php
@@ -24,7 +24,7 @@ $LongueurMax = 12;
 	    					if($case->getCouleur() == "service" || $case->getCouleur() =="train") :?>
 	    						<td class="caseH">
 			    					<svg width="75px" height="100px" xmlns="http://www.w3.org/2000/svg" version="1.1">
-										<text class="titreP" x="38px" y="15px" transform="rotate(180 38,35)">
+										<text class="titreP" x="38px" y="5px" transform="rotate(180 38,35)">
 											<?php $nomCase = $case->getNom();
 											$dernierEspace =0;
 											$compteurEsp =0; 
@@ -35,24 +35,24 @@ $LongueurMax = 12;
 												endwhile;
 												if(strlen(substr($nomCase, 0, $dernierEspace)) > $LongueurMax) :
 													echo substr(substr($nomCase, 0, $dernierEspace), 0, strpos(substr($nomCase, 0, $dernierEspace), ' '));?>
-													<tspan class="titreP" x="38px" y="25px"> <?php echo substr(substr($nomCase, 0, $dernierEspace), strpos(substr($nomCase, 0, $dernierEspace), ' '), $dernierEspace);?></tspan>
-													<tspan class="titreP" x="38px" y="35px"><?php echo substr($nomCase, $dernierEspace + 1);?></tspan>
+													<tspan class="titreP" x="38px" y="15px"> <?php echo substr(substr($nomCase, 0, $dernierEspace), strpos(substr($nomCase, 0, $dernierEspace), ' '), $dernierEspace);?></tspan>
+													<tspan class="titreP" x="38px" y="25px"><?php echo substr($nomCase, $dernierEspace + 1);?></tspan>
 												<?php else :
 													echo substr($nomCase, 0, $dernierEspace);?>
-													<tspan class="titreP" x="38px" y="25px"><?php echo substr($nomCase, $dernierEspace + 1);?></tspan>
+													<tspan class="titreP" x="38px" y="15px"><?php echo substr($nomCase, $dernierEspace + 1);?></tspan>
 												
 												<?php endif;
 												 else :
 												 	 echo $case->getNom();
 												 endif;?></text>
-										<text class="montant" x="38px" y="65px" transform="rotate(180 38,35)"><?php echo $case->getPrix();?> $</text>
+										<text class="montant" x="38px" y="55px" transform="rotate(180 38,35)"><?php echo $case->getPrix();?> $</text>
 									</svg>
 								</td>
 							<?php else :?>
 			    				<td class="caseH">
 			    					<svg width="75px" height="100px" xmlns="http://www.w3.org/2000/svg" version="1.1">
 										<rect x="0px" y="70px" width="75px" height="30px" style="fill:<?php echo $case->getCouleurHTML();?>;"/>
-										<text class="titreP" x="38px" y="15px" transform="rotate(180 38,35)">
+										<text class="titreP" x="38px" y="5px" transform="rotate(180 38,35)">
 											<?php $nomCase = $case->getNom();
 											$dernierEspace =0;
 											$compteurEsp =0; 
@@ -63,17 +63,17 @@ $LongueurMax = 12;
 												endwhile;
 												if(strlen(substr($nomCase, 0, $dernierEspace)) > $LongueurMax) :
 													echo substr(substr($nomCase, 0, $dernierEspace), 0, strpos(substr($nomCase, 0, $dernierEspace), ' '));?>
-													<tspan class="titreP" x="38px" y="25px"> <?php echo substr(substr($nomCase, 0, $dernierEspace), strpos(substr($nomCase, 0, $dernierEspace), ' '), $dernierEspace);?></tspan>
-													<tspan class="titreP" x="38px" y="35px"><?php echo substr($nomCase, $dernierEspace + 1);?></tspan>
+													<tspan class="titreP" x="38px" y="15px"> <?php echo substr(substr($nomCase, 0, $dernierEspace), strpos(substr($nomCase, 0, $dernierEspace), ' '), $dernierEspace);?></tspan>
+													<tspan class="titreP" x="38px" y="25px"><?php echo substr($nomCase, $dernierEspace + 1);?></tspan>
 												<?php else :
 													echo substr($nomCase, 0, $dernierEspace);?>
-													<tspan class="titreP" x="38px" y="25px"><?php echo substr($nomCase, $dernierEspace + 1);?></tspan>
+													<tspan class="titreP" x="38px" y="15px"><?php echo substr($nomCase, $dernierEspace + 1);?></tspan>
 												
 													<?php endif;
 												 else :?>
 												 	<?php echo $case->getNom();
 												 endif;?></text>
-										<text class="montant" x="38px" y="65px" transform="rotate(180 38,35)"><?php echo $case->getPrix();?> $</text>
+										<text class="montant" x="38px" y="55px" transform="rotate(180 38,35)"><?php echo $case->getPrix();?> $</text>
 									</svg>
 								</td>
 							<?php endif;?>
@@ -87,7 +87,7 @@ $LongueurMax = 12;
 	    			if ($case == null) :?>
 		    				<td class="caseCote">
 								<svg width="100px" height="75px" xmlns="http://www.w3.org/2000/svg" version="1.1">
-									<text class="titreP" x="38px" y="15px" transform="rotate(90 38,35)">
+									<text class="titreP" x="38px" y="5px" transform="rotate(90 38,35)">
 										Case action</text>
 								</svg>
 							</td> 
@@ -95,7 +95,7 @@ $LongueurMax = 12;
 	    					if($case->getCouleur() == "service" || $case->getCouleur() =="train") :?> 
 	    					<td class="caseCote">
 		    					<svg width="100px" height="75px" xmlns="http://www.w3.org/2000/svg" version="1.1">
-									<text class="titreP" x="38px" y="15px" transform="rotate(90 38,35)">
+									<text class="titreP" x="38px" y="5px" transform="rotate(90 38,35)">
 										<?php $nomCase = $case->getNom();
 											$dernierEspace =0;
 											$compteurEsp =0; 
@@ -106,24 +106,24 @@ $LongueurMax = 12;
 												endwhile;
 												if(strlen(substr($nomCase, 0, $dernierEspace)) > $LongueurMax) :
 													echo substr(substr($nomCase, 0, $dernierEspace), 0, strpos(substr($nomCase, 0, $dernierEspace), ' '));?>
-													<tspan class="titreP" x="38px" y="25px"> <?php echo substr(substr($nomCase, 0, $dernierEspace), strpos(substr($nomCase, 0, $dernierEspace), ' '), $dernierEspace);?></tspan>
-													<tspan class="titreP" x="38px" y="35px"><?php echo substr($nomCase, $dernierEspace + 1);?></tspan>
+													<tspan class="titreP" x="38px" y="15px"> <?php echo substr(substr($nomCase, 0, $dernierEspace), strpos(substr($nomCase, 0, $dernierEspace), ' '), $dernierEspace);?></tspan>
+													<tspan class="titreP" x="38px" y="25px"><?php echo substr($nomCase, $dernierEspace + 1);?></tspan>
 												<?php else :
 													echo substr($nomCase, 0, $dernierEspace);?>
-													<tspan class="titreP" x="38px" y="25px"><?php echo substr($nomCase, $dernierEspace + 1);?></tspan>
+													<tspan class="titreP" x="38px" y="15px"><?php echo substr($nomCase, $dernierEspace + 1);?></tspan>
 												
 												<?php endif;
 												 else :?>
 												 <?php echo $case->getNom();
 												 endif;?></text>
-									<text class="montant" x="38px" y="65px" transform="rotate(90 38,35)"><?php echo $case->getPrix();?> $</text>
+									<text class="montant" x="38px" y="55px" transform="rotate(90 38,35)"><?php echo $case->getPrix();?> $</text>
 								</svg>
 							</td>
 							<?php else: ?>
 		    				<td class="caseCote">
 		    					<svg width="100px" height="75px" xmlns="http://www.w3.org/2000/svg" version="1.1">
 									<rect x="70px" y="0px" width="30px" height="75px" style="fill:<?php echo $case->getCouleurHTML();?>;"/>
-									<text class="titreP" x="38px" y="15px" transform="rotate(90 38,35)">
+									<text class="titreP" x="38px" y="5px" transform="rotate(90 38,35)">
 										<?php $nomCase = $case->getNom();
 											$dernierEspace =0;
 											$compteurEsp =0; 
@@ -134,17 +134,17 @@ $LongueurMax = 12;
 												endwhile;
 												if(strlen(substr($nomCase, 0, $dernierEspace)) > $LongueurMax) :
 													echo substr(substr($nomCase, 0, $dernierEspace), 0, strpos(substr($nomCase, 0, $dernierEspace), ' '));?>
-													<tspan class="titreP" x="38px" y="25px"> <?php echo substr(substr($nomCase, 0, $dernierEspace), strpos(substr($nomCase, 0, $dernierEspace), ' '), $dernierEspace);?></tspan>
-													<tspan class="titreP" x="38px" y="35px"><?php echo substr($nomCase, $dernierEspace + 1);?></tspan>
+													<tspan class="titreP" x="38px" y="15px"> <?php echo substr(substr($nomCase, 0, $dernierEspace), strpos(substr($nomCase, 0, $dernierEspace), ' '), $dernierEspace);?></tspan>
+													<tspan class="titreP" x="38px" y="25px"><?php echo substr($nomCase, $dernierEspace + 1);?></tspan>
 												<?php else :
 													echo substr($nomCase, 0, $dernierEspace);?>
-													<tspan class="titreP" x="38px" y="25px"><?php echo substr($nomCase, $dernierEspace + 1);?></tspan>
+													<tspan class="titreP" x="38px" y="15px"><?php echo substr($nomCase, $dernierEspace + 1);?></tspan>
 												
 												<?php endif;
 												else :?>
 												 <?php echo $case->getNom();
 												 endif;?></text>
-									<text class="montant" x="38px" y="65px" transform="rotate(90 38,35)"><?php echo $case->getPrix();?> $</text>
+									<text class="montant" x="38px" y="55px" transform="rotate(90 38,35)"><?php echo $case->getPrix();?> $</text>
 								</svg>
 							</td>
 							<?php endif;?>
@@ -156,7 +156,7 @@ $LongueurMax = 12;
   									<text x="130" y="185" width="100" height="150" transform="rotate(130 150,150)">CHANCE</text>
   									<rect x="-50" y="0" rx="20" ry="20" width="120" height="420" stroke="white" stroke-width="2" fill="red" transform="rotate(220 200,200)" />
   									<rect x="-40" y="10" rx="20" ry="20" width="100" height="400" stroke="white" stroke-width="2" fill="red" transform="rotate(220 200,200)" />
-  									<text x="150" y="430" width="100" height="150" style="fill:white; stroke:black; font-size:52px;"transform="rotate(310 150,150)">MONOPOLY</text>
+  									<text x="160" y="385" width="100" height="150" style="fill:white; stroke:black; font-size:52px;"transform="rotate(310 150,150)">MONOPOLY</text>
   									<rect x="-350" y="75" width="100" height="150" stroke="black" stroke-width="2" stroke-dasharray="3" fill="RGB(203,233,225)" transform="rotate(220 200,200)"/>
   									<text x="95" y="725" width="100" height="150" transform="rotate(310 150,150)">Caisse Commune </text>
 								</svg> 
@@ -167,7 +167,7 @@ $LongueurMax = 12;
 	    					if ($case == null) :?>
 			    				<td class="caseCote">
 									<svg width="100px" height="75px" xmlns="http://www.w3.org/2000/svg" version="1.1">
-									<text class="titreP" x="38px" y="15px" transform="rotate(270 38,35)">
+									<text class="titreP" x="38px" y="5px" transform="rotate(270 38,35)">
 										Case action</text>
 								</svg>
 								</td> 
@@ -175,7 +175,7 @@ $LongueurMax = 12;
 	    						<?php if($case->getCouleur() == "service" || $case->getCouleur() == "train") :?>
 		    						<td class="caseCote">
 				    					<svg width="100px" height="75px" xmlns="http://www.w3.org/2000/svg" version="1.1">
-											<text class="titreP" x="38px" y="40px" transform="rotate(270 38,35)">
+											<text class="titreP" x="38px" y="30px" transform="rotate(270 38,35)">
 												<?php $nomCase = $case->getNom();
 											$dernierEspace =0;
 											$compteurEsp =0; 
@@ -186,24 +186,24 @@ $LongueurMax = 12;
 												endwhile;
 												if(strlen(substr($nomCase, 0, $dernierEspace)) > $LongueurMax) :
 													echo substr(substr($nomCase, 0, $dernierEspace), 0, strpos(substr($nomCase, 0, $dernierEspace), ' '));?>
-													<tspan class="titreP" x="38px" y="50px"> <?php echo substr(substr($nomCase, 0, $dernierEspace), strpos(substr($nomCase, 0, $dernierEspace), ' '), $dernierEspace);?></tspan>
-													<tspan class="titreP" x="38px" y="60px"><?php echo substr($nomCase, $dernierEspace + 1);?></tspan>
+													<tspan class="titreP" x="38px" y="40px"> <?php echo substr(substr($nomCase, 0, $dernierEspace), strpos(substr($nomCase, 0, $dernierEspace), ' '), $dernierEspace);?></tspan>
+													<tspan class="titreP" x="38px" y="50px"><?php echo substr($nomCase, $dernierEspace + 1);?></tspan>
 												<?php else :
 													echo substr($nomCase, 0, $dernierEspace);?>
-													<tspan class="titreP" x="38px" y="50px"><?php echo substr($nomCase, $dernierEspace + 1);?></tspan>
+													<tspan class="titreP" x="38px" y="40px"><?php echo substr($nomCase, $dernierEspace + 1);?></tspan>
 												
 												<?php endif;
 												 else :?>
 												 <?php echo $case->getNom();
 												 endif;?></text>
-											<text class="montant" x="38px" y="90px" transform="rotate(270 38,35)"><?php echo $case->getPrix();?> $</text>
+											<text class="montant" x="38px" y="80px" transform="rotate(270 38,35)"><?php echo $case->getPrix();?> $</text>
 										</svg>
 									</td>
 								<?php else :?>
 				    				<td class="caseCote">
 				    					<svg width="100px" height="75px" xmlns="http://www.w3.org/2000/svg" version="1.1">
 											<rect x="0px" y="0px" width="30px" height="75px" style="fill:<?php echo $case->getCouleurHTML();?>;"/>
-											<text class="titreP" x="38px" y="40px" transform="rotate(270 38,35)">
+											<text class="titreP" x="38px" y="30px" transform="rotate(270 38,35)">
 												<?php $nomCase = $case->getNom();
 											$dernierEspace =0;
 											$compteurEsp =0; 
@@ -214,17 +214,17 @@ $LongueurMax = 12;
 												endwhile;
 												if(strlen(substr($nomCase, 0, $dernierEspace)) > $LongueurMax) :
 													echo substr(substr($nomCase, 0, $dernierEspace), 0, strpos(substr($nomCase, 0, $dernierEspace), ' '));?>
-													<tspan class="titreP" x="38px" y="50px"> <?php echo substr(substr($nomCase, 0, $dernierEspace), strpos(substr($nomCase, 0, $dernierEspace), ' '), $dernierEspace);?></tspan>
-													<tspan class="titreP" x="38px" y="60px"><?php echo substr($nomCase, $dernierEspace + 1);?></tspan>
+													<tspan class="titreP" x="38px" y="40px"> <?php echo substr(substr($nomCase, 0, $dernierEspace), strpos(substr($nomCase, 0, $dernierEspace), ' '), $dernierEspace);?></tspan>
+													<tspan class="titreP" x="38px" y="50px"><?php echo substr($nomCase, $dernierEspace + 1);?></tspan>
 												<?php else :
 													echo substr($nomCase, 0, $dernierEspace);?>
-													<tspan class="titreP" x="38px" y="50px"><?php echo substr($nomCase, $dernierEspace + 1);?></tspan>
+													<tspan class="titreP" x="38px" y="40px"><?php echo substr($nomCase, $dernierEspace + 1);?></tspan>
 												
 												<?php endif;
 												 else :?>
 												 <?php echo $case->getNom();
 												 endif;?></text>
-											<text class="montant" x="38px" y="90px" transform="rotate(270 38,35)"><?php echo $case->getPrix();?> $</text>
+											<text class="montant" x="38px" y="80px" transform="rotate(270 38,35)"><?php echo $case->getPrix();?> $</text>
 										</svg>
 									</td>
 								<?php endif;?>
@@ -253,7 +253,7 @@ $LongueurMax = 12;
 	    					if($case->getCouleur() == "service" || $case->getCouleur() =="train") :?>
 		    					<td class="caseH">
 			    					<svg width="75px" height="100px" xmlns="http://www.w3.org/2000/svg" version="1.1">
-										<text class="titreP" x="38px" y="40px">
+										<text class="titreP" x="38px" y="30px">
 											<?php $nomCase = $case->getNom();
 											$dernierEspace =0;
 											$compteurEsp =0; 
@@ -264,24 +264,24 @@ $LongueurMax = 12;
 												endwhile;
 												if(strlen(substr($nomCase, 0, $dernierEspace)) > $LongueurMax) :
 													echo substr(substr($nomCase, 0, $dernierEspace), 0, strpos(substr($nomCase, 0, $dernierEspace), ' '));?>
-													<tspan class="titreP" x="38px" y="50px"> <?php echo substr(substr($nomCase, 0, $dernierEspace), strpos(substr($nomCase, 0, $dernierEspace), ' '), $dernierEspace);?></tspan>
-													<tspan class="titreP" x="38px" y="60px"><?php echo substr($nomCase, $dernierEspace + 1);?></tspan>
+													<tspan class="titreP" x="38px" y="40px"> <?php echo substr(substr($nomCase, 0, $dernierEspace), strpos(substr($nomCase, 0, $dernierEspace), ' '), $dernierEspace);?></tspan>
+													<tspan class="titreP" x="38px" y="50px"><?php echo substr($nomCase, $dernierEspace + 1);?></tspan>
 												<?php else :
 													echo substr($nomCase, 0, $dernierEspace);?>
-													<tspan class="titreP" x="38px" y="50px"><?php echo substr($nomCase, $dernierEspace + 1);?></tspan>
+													<tspan class="titreP" x="38px" y="40px"><?php echo substr($nomCase, $dernierEspace + 1);?></tspan>
 												
 												<?php endif;
 												 else :?>
 												 <?php echo $case->getNom();
 												 endif;?></text>
-										<text class="montant" x="38px" y="90px"><?php echo $case->getPrix();?> $</text>
+										<text class="montant" x="38px" y="80px"><?php echo $case->getPrix();?> $</text>
 									</svg>
 								</td>
 	    				<?php else :?>
 		    				<td class="caseH">
 		    					<svg width="75px" height="100px" xmlns="http://www.w3.org/2000/svg" version="1.1">
 									<rect x="0px" y="0px" width="75px" height="30px" style="fill:<?php echo $case->getCouleurHTML();?>;"/>
-									<text class="titreP" x="38px" y="40px">
+									<text class="titreP" x="38px" y="30px">
 										<?php $nomCase = $case->getNom();
 											$dernierEspace =0;
 											$compteurEsp =0; 
@@ -292,17 +292,17 @@ $LongueurMax = 12;
 												endwhile;
 												if(strlen(substr($nomCase, 0, $dernierEspace)) > $LongueurMax) :
 													echo substr(substr($nomCase, 0, $dernierEspace), 0, strpos(substr($nomCase, 0, $dernierEspace), ' '));?>
-													<tspan class="titreP" x="38px" y="50px"> <?php echo substr(substr($nomCase, 0, $dernierEspace), strpos(substr($nomCase, 0, $dernierEspace), ' '), $dernierEspace);?></tspan>
-													<tspan class="titreP" x="38px" y="60px"><?php echo substr($nomCase, $dernierEspace + 1);?></tspan>
+													<tspan class="titreP" x="38px" y="40px"> <?php echo substr(substr($nomCase, 0, $dernierEspace), strpos(substr($nomCase, 0, $dernierEspace), ' '), $dernierEspace);?></tspan>
+													<tspan class="titreP" x="38px" y="50px"><?php echo substr($nomCase, $dernierEspace + 1);?></tspan>
 												<?php else :
 													echo substr($nomCase, 0, $dernierEspace);?>
-													<tspan class="titreP" x="38px" y="50px"><?php echo substr($nomCase, $dernierEspace + 1);?></tspan>
+													<tspan class="titreP" x="38px" y="40px"><?php echo substr($nomCase, $dernierEspace + 1);?></tspan>
 												
 												<?php endif;
 												else :?>
 												 <?php echo $case->getNom();
 												 endif;?></text>
-									<text class="montant" x="38px" y="90px"><?php echo $case->getPrix();?> $</text>
+									<text class="montant" x="38px" y="80px"><?php echo $case->getPrix();?> $</text>
 								</svg>
 							</td>
 						<?php endif;?>
