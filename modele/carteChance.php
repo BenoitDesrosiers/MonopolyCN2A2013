ca68467e80f2ce20f5d374b1c01ec964da619ab7
CacaMasta <banane126@yahoo.fr>
2013-12-11 17:26:33 -0500

    extra extra

--- modele/carteChance.php
+++ modele/carteChance.php
@@ -4,6 +4,8 @@ require_once "modele/carte.php";
 require_once "modele/paiementAuxJoueurs.php";
 require_once "interface/entreposageDatabase.php";
 require_once "dataMapper/carteChanceDataMapper.php";
+require_once "modele/joueur.php";
+require_once "modele/caseDeJeu.php";
 
 
 class CarteChance extends Carte{
@@ -27,12 +29,35 @@ class CarteChance extends Carte{
         $this->getDataMapper()->insert($this);
     }
     
-    public function execute($joueur){
+    public function execute(Joueur $joueur){
         switch($this->getActionID()){
             case 29:
                 $xyz = new PaiementAuxJoueurs();
                 $xyz->execute($joueur, 50);
                 break;
+            case 24:
+            	$joueur->setPosition($joueur->getPosition()-3); //nouvelle position du joueur
+            	
+            	$partie = Partie::parId($joueur->getPartieId()); //4 lignes de code qui cause "l'atterissage" du joueur sur la nouvelle case
+            	$tableau = $partie->getTableau();				 //J'assume que la version du code que j'ai est à jour, sinon la partie "atterir sur" devra être mise à jour
+            	$case = $tableau->getCaseParPosition($joueur->getPosition());
+            	$case->atterrirSur($joueur);
+            	
+            	break;
+            case 27:
+            	$joueur->setPosition(CaseDeJeuAchetable::parId(5)->getPosition()); //nouvelle position du joueur
+            	
+            	$partie = Partie::parId($joueur->getPartieId()); //4 lignes de code qui cause "l'atterissage" du joueur sur la nouvelle case
+            	$tableau = $partie->getTableau();				 //J'assume que la version du code que j'ai est à jour, sinon la partie "atterir sur" devra être mise à jour
+            	$case = $tableau->getCaseParPosition($joueur->getPosition());
+            	$case->atterrirSur($joueur);
+            	
+            	if ($joueur->getPosition()>CaseDeJeuAchetable::parId(5)->getPosition()) { //si le joueur est plus "loin" que reading, il passe forcement par go donc encaisse 200$
+            		$joueur->encaisse(200);
+            	}
+            	
+            	break;
+            	
             // Ajouter les autres actions des cartes chances
         }
     }
